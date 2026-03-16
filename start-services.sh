1051f371de65366b3b21b01e5b1cda1156f07cf4
LeoPro23 <lguimaray@unitru.edu.pe>
2026-03-16 05:58:11 -0500

    chore: Add `ls -R dist` command to start script for build verification.

--- start-services.sh
+++ start-services.sh
@@ -7,5 +7,7 @@ python3 -m uvicorn main:app --host 0.0.0.0 --port 8001 &
 
 # Volver a la raíz y arrancar la aplicación Node.js en primer plano
 cd /app
+echo "Contenido de dist:"
+ls -R dist
 echo "Iniciando servidor principal NestJS..."
 npm run start:prod
